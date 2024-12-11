d90fe0105de89dff313114b8f83f0b95303e7b42
cocktailpeanut <cocktailpeanuts@proton.me>
2024-12-10 21:51:25 -0500

    update

--- install.js
+++ install.js
@@ -20,6 +20,18 @@ module.exports = {
         ]
       }
     },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip uninstall torch",
+          "pip uninstall torchvision",
+        ]
+      }
+    },
     // Delete this step if your project does not use torch
     {
       method: "script.start",
