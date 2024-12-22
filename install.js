657621a35a42950604e2c275b7dfc4a14652f336
cocktailpeanut <cocktailpeanuts@proton.me>
2024-12-22 13:47:51 -0500

    update

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/peanutcocktail/HunyuanVideoGP app",
+          "git clone https://github.com/deepbeepmeep/HunyuanVideoGP app",
         ]
       }
     },
