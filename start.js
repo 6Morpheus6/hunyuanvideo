8963bed91bafaf12571e538224bd9a8c2d3f2d5a
cocktailpeanut <cocktailpeanuts@proton.me>
2024-12-24 05:03:03 -0500

    update

--- start.js
+++ start.js
@@ -1,34 +1,41 @@
-module.exports = {
-  daemon: true,
-  run: [
-    {
-      method: "shell.run",
-      params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          "python gradio_server.py --profile 4",    // Edit with your custom commands
-        ],
-        on: [{
-          // The regular expression pattern to monitor.
-          // When this pattern occurs in the shell terminal, the shell will return,
-          // and the script will go onto the next step.
-          "event": "/http:\/\/[0-9.:]+/",   
+module.exports = async (kernel) => {
+  let port = await kernel.port()
+  return {
+    daemon: true,
+    run: [
+      {
+        method: "shell.run",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          env: {
+            SERVER_NAME: "127.0.0.1",
+            SERVER_PORT: port
+          },
+          path: "app",                // Edit this to customize the path to start the shell from
+          message: [
+            "python gradio_server.py",    // Edit with your custom commands
+          ],
+          on: [{
+            // The regular expression pattern to monitor.
+            // When this pattern occurs in the shell terminal, the shell will return,
+            // and the script will go onto the next step.
+            "event": "/http:\/\/[0-9.:]+/",   
 
-          // "done": true will move to the next step while keeping the shell alive.
-          // "kill": true will move to the next step after killing the shell.
-          "done": true
-        }]
+            // "done": true will move to the next step while keeping the shell alive.
+            // "kill": true will move to the next step after killing the shell.
+            "done": true
+          }]
+        }
+      },
+      {
+        // This step sets the local variable 'url'.
+        // This local variable will be used in pinokio.js to display the "Open WebUI" tab when the value is set.
+        method: "local.set",
+        params: {
+          // the input.event is the regular expression match object from the previous step
+          url: "{{input.event[0]}}"
+        }
       }
-    },
-    {
-      // This step sets the local variable 'url'.
-      // This local variable will be used in pinokio.js to display the "Open WebUI" tab when the value is set.
-      method: "local.set",
-      params: {
-        // the input.event is the regular expression match object from the previous step
-        url: "{{input.event[0]}}"
-      }
-    }
-  ]
+    ]
+  }
 }
