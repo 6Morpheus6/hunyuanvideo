28025db127cfb899f6f1ce3338ca0a0e946211f5
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-10 07:23:22 -0500

    update

--- torch.js
+++ torch.js
@@ -11,7 +11,7 @@ module.exports = {
           "uv pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 {{args && args.xformers ? 'xformers' : ''}}  --index-url https://download.pytorch.org/whl/cu121 --force-reinstall",
           //"uv pip install https://github.com/bdashore3/flash-attention/releases/download/v2.7.1.post1/flash_attn-2.7.1.post1+cu124torch2.5.1cxx11abiFALSE-cp310-cp310-win_amd64.whl"
           "uv pip install https://github.com/sdbds/SageAttention-for-windows/releases/download/2.0.1/sageattention-2.0.1+cu124torch2.5.1-cp310-cp310-win_amd64.whl",
-          "uv pip install https://github.com/woct0rdho/triton-windows/releases/download/v3.1.0-windows.post9/triton-3.1.0-cp311-cp311-win_amd64.whl"
+          "uv pip install https://github.com/PrashantSaikia/Triton-for-Windows/blob/main/triton-2.0.0-cp310-cp310-win_amd64.whl"
         ]
       }
     },
